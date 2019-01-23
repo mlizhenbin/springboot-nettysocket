731fde8c9000c68f8ed7a990e73898be099ff6b9
lizhenbin08@sina.com <125906862mac>
2019-01-23 17:38:53 +0800

    toy test

--- src/test/java/org/lzbruby/toy/SortTest.java
+++ src/test/java/org/lzbruby/toy/SortTest.java
@@ -15,6 +15,35 @@ public class SortTest {
      */
     private static final Logger logger = LoggerFactory.getLogger(SortTest.class);
 
+    /**
+     * 插入排序
+     */
+    @Test
+    public void insertionSort() {
+        int[] v = {2, 3, 1, 3, 2, 6, 7, 8, 23, 27, 2, 3, 1, 0, 20};
+        logger.info("冒泡排序：小——>大");
+        for (int i = 1; i < v.length; i++) {
+            //只能从当前索引往前循环，因为索引前的数组皆为有序的，索引只要确定当前索引的数据的为止即可
+            for (int j = i; j > 0 && v[j] < v[j - 1]; j--) {
+                int temp = v[j];
+                v[j] = v[j - 1];
+                v[j - 1] = temp;
+            }
+            logger.info("index={}, v={}", i, ArrayUtils.toString(v));
+        }
+
+        int[] v1 = {2, 3, 1, 3, 2, 6, 7, 8, 23, 27, 2, 3, 1, 0, 20};
+        logger.info("冒泡排序：小——>大");
+        for (int i = 1; i < v1.length; i++) {
+            //只能从当前索引往前循环，因为索引前的数组皆为有序的，索引只要确定当前索引的数据的为止即可
+            for (int j = i; j > 0 && v1[j] > v1[j - 1]; j--) {
+                int temp = v1[j];
+                v1[j] = v1[j - 1];
+                v1[j - 1] = temp;
+            }
+            logger.info("index={}, v1={}", i, ArrayUtils.toString(v1));
+        }
+    }
 
     /**
      * 冒泡
